937714d2a42a5e6f3158d04608c9a39e2df033c2
lilylemonoh <lilylemonoh@naver.com>
2024-02-19 21:14:59 +0900

    섹션 2 - 영속성 관리 - 내부 동작 방식

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -16,22 +16,30 @@ public class JpaMain {
         tx.begin();
 
         try {
-            //code - 회원 저장
+//            //code - 회원 저장
+//            Member member = new Member();
+//            member.setId(1L);
+//            member.setName("helloA");
+//            em.persist(member);
+//
+//            // code - 회원  수정
+//            Member findMember = em.find(Member.class, 1L);
+//            findMember.setName("HelloJPA"); // 같은 트랜잭션 안에 있으면
+//
+//            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+//                    .getResultList();
+//
+//            for (Member members : result) {
+//                System.out.println("member.name = " + member.getName());
+//            }
+
+            // 비영속
             Member member = new Member();
-            member.setId(1L);
-            member.setName("helloA");
+            member.setId(100L);
+            member.setName("HelloJPA");
+
+            // 영속
             em.persist(member);
-            
-            // code - 회원  수정
-            Member findMember = em.find(Member.class, 1L);
-            findMember.setName("HelloJPA"); // 같은 트랜잭션 안에 있으면
-
-            List<Member> result = em.createQuery("select m from Member as m", Member.class)
-                    .getResultList();
-
-            for (Member members : result) {
-                System.out.println("member.name = " + member.getName());
-            }
 
             tx.commit();
         } catch (Exception e) {
