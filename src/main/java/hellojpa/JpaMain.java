698b147f078c0b7c653053689371b3bceef9963f
lilylemonoh <lilylemonoh@naver.com>
2024-02-27 06:28:13 +0900

    섹션 4 - 엔티티 매핑 : 필드와 컬럼 매핑

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -16,59 +16,6 @@ public class JpaMain {
         tx.begin();
 
         try {
-//            //code - 회원 저장
-//            Member member = new Member();
-//            member.setId(1L);
-//            member.setName("helloA");
-//            em.persist(member);
-//
-//            // code - 회원  수정
-//            Member findMember = em.find(Member.class, 1L);
-//            findMember.setName("HelloJPA"); // 같은 트랜잭션 안에 있으면
-//
-//            List<Member> result = em.createQuery("select m from Member as m", Member.class)
-//                    .getResultList();
-//
-//            for (Member members : result) {
-//                System.out.println("member.name = " + member.getName());
-//            }
-
-            // 비영속
-//            Member member = new Member();
-//            member.setId(101L);
-//            member.setName("HelloJPA");
-//
-//            // 영속
-//            System.out.println("===Before===");
-//            em.persist(member);
-//            System.out.println("===After===");
-
-//            Member findMember = em.find(Member.class, 101L);
-//
-//            System.out.println("findMember.getId = " +findMember.getId());
-//            System.out.println("findMember.getName = " + findMember.getName());
-
-            // 영속 엔티티의 동일성 보장
-//            Member findMember1 = em.find(Member.class, 101L);
-//            Member findMember2 = em.find(Member.class, 101L);
-//            System.out.println(findMember1 == findMember2);
-
-            // 영속
-//            Member member1 = new Member(150L, "A");
-//            Member member2 = new Member(160L, "B");
-//
-//            em.persist(member1);
-//            em.persist(member2);
-
-//            System.out.println("===================");
-//            Member member = em.find(Member.class, 101L);
-//            member.setName("ZZZZZZZZZZZZZ");
-            Member member = new Member(200L, "member200");
-
-            member.setName("AAAA");
-            em.detach(member);
-
-            System.out.println("===========");
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -2,38 +2,34 @@ package hellojpa;
 
 import jakarta.persistence.*;
 
+import java.util.Date;
+
 @Entity
-//@Table(name = "USER")
+//@Table(uniqueConstraints = )
 public class Member {
 
     @Id
     private Long id;
 
-//    @Column(name = "username")
-    private String name;
-    private int age;
+    @Column(name = "name", nullable = false, columnDefinition = "varchar(100) default 'EMPTY'")
+    private String username;
+    private Integer age;
 
-    public Member(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
+    @Enumerated(EnumType.STRING) // 주의! ORDINAL(default) 사용 x
+    private RoleType roleType;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+
+    @Lob
+    private String description;
+
+    @Transient
+    private int temp;
 
     public Member() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 }
