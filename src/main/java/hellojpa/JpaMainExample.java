470bd2e1042d0e85080a0ded4f7d07d0d1cff50d
lilylemonoh <lilylemonoh@naver.com>
2024-03-03 13:29:49 +0900

    섹션 8 - 프록시와 연관관계 정리 - 프록시

--- src/main/java/hellojpa/JpaMainExample.java
+++ src/main/java/hellojpa/JpaMainExample.java
@@ -1,9 +1,7 @@
 package hellojpa;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.EntityTransaction;
-import jakarta.persistence.Persistence;
+import jakarta.persistence.*;
+import org.hibernate.Hibernate;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -20,22 +18,33 @@ public class JpaMainExample {
           tx.begin();
 
           try {
-              MemberExample member = new MemberExample();
-              member.setUsername("user1");
-              member.setCreatedBy("kim");
-              member.setCreatedDate(LocalDateTime.now());
 
-              em.persist(member);
+              MemberExample member1 = new MemberExample();
+              member1.setUsername("member1");
+              em.persist(member1);
 
               em.flush();
               em.clear();
 
+              MemberExample refMember = em.getReference(MemberExample.class, member1.getId());
+              System.out.println("refMember = " + refMember.getClass()); // Proxy
+//              refMember.getUsername(); // 강제 초기화
+
+              Hibernate.initialize(refMember); // 강제 초기화
+              // 참고 : JPA 표준은 강제 초기화 없음
+              // 강제 호출: member.getName()
+
+//              System.out.println("isLoaded = " + emf.getPersistenceUnitUtil().isLoaded(refMember));
+
               tx.commit();
           } catch (Exception e) {
               tx.rollback();
+              e.printStackTrace();
           } finally {
               em.close();
           }
           emf.close();
     }
+
+
 }
--- src/main/java/hellojpa/MemberExample.java
+++ src/main/java/hellojpa/MemberExample.java
@@ -22,14 +22,36 @@ public class MemberExample extends BaseEntityExample {
     // 다대일 양방향을 사용하자
     private Team team;
 
-    @OneToOne
-    @JoinColumn(name = "LOCKER_ID")
-    private Locker locker;
+//    @OneToOne
+//    @JoinColumn(name = "LOCKER_ID")
+//    private Locker locker;
+//
+//    @OneToMany(mappedBy = "member")
+//    private List<MemberProduct> memberProducts = new ArrayList<>();
 
-    @OneToMany(mappedBy = "member")
-    private List<MemberProduct> memberProducts = new ArrayList<>();
+    public Team getTeam() {
+        return team;
+    }
 
+    public void setTeam(Team team) {
+        this.team = team;
+    }
 
+//    public Locker getLocker() {
+//        return locker;
+//    }
+//
+//    public void setLocker(Locker locker) {
+//        this.locker = locker;
+//    }
+//
+//    public List<MemberProduct> getMemberProducts() {
+//        return memberProducts;
+//    }
+//
+//    public void setMemberProducts(List<MemberProduct> memberProducts) {
+//        this.memberProducts = memberProducts;
+//    }
 
     public Long getId() {
         return id;
