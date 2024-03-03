b2369a7420030b307c2389099cef93e84f6e40c9
lilylemonoh <lilylemonoh@naver.com>
2024-03-03 14:53:28 +0900

    섹션 8 - 프록시와 연관관계 정리 - 즉시 로딩과 지연 로딩

--- src/main/java/hellojpa/JpaMainExample.java
+++ src/main/java/hellojpa/JpaMainExample.java
@@ -18,23 +18,36 @@ public class JpaMainExample {
           tx.begin();
 
           try {
+              Team team = new Team();
+              team.setName("teamA");
+              em.persist(team);
+
+              Team teamB = new Team();
+              teamB.setName("teamB");
+              em.persist(teamB);
 
               MemberExample member1 = new MemberExample();
               member1.setUsername("member1");
+              member1.setTeam(team);
               em.persist(member1);
+
+              MemberExample member2 = new MemberExample();
+              member2.setUsername("member2");
+              member2.setTeam(teamB);
+              em.persist(member2);
 
               em.flush();
               em.clear();
 
-              MemberExample refMember = em.getReference(MemberExample.class, member1.getId());
-              System.out.println("refMember = " + refMember.getClass()); // Proxy
-//              refMember.getUsername(); // 강제 초기화
+//              MemberExample m  = em.find(MemberExample.class, member1.getId());
 
-              Hibernate.initialize(refMember); // 강제 초기화
-              // 참고 : JPA 표준은 강제 초기화 없음
-              // 강제 호출: member.getName()
+              List<MemberExample> members = em.createQuery(
+                      "select m from MemberExample m join fetch m.team",
+                              MemberExample.class)
+                      .getResultList();
 
-//              System.out.println("isLoaded = " + emf.getPersistenceUnitUtil().isLoaded(refMember));
+              // SQL: select * from Member
+              // SQL: select * from Team where TEAM_ID = xxx
 
               tx.commit();
           } catch (Exception e) {
--- src/main/java/hellojpa/MemberExample.java
+++ src/main/java/hellojpa/MemberExample.java
@@ -15,11 +15,8 @@ public class MemberExample extends BaseEntityExample {
     @Column(name = "USERNAME")
     private String username;
 
-    @ManyToOne
-    @JoinColumn(name = "TEAM_ID", insertable = false, updatable = false)
-    // 이런 매핑은 공식적으로 존재하지 않음
-    // 읽기 전용 필드를 사용해서 양방향처럼 사용하는 방법
-    // 다대일 양방향을 사용하자
+    @ManyToOne (fetch = FetchType.LAZY)
+    @JoinColumn
     private Team team;
 
 //    @OneToOne
