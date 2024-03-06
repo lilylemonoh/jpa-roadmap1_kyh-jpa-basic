449f99528e1951e8103ecc495fde912aa17d03ce
lilylemonoh <lilylemonoh@naver.com>
2024-03-06 20:05:03 +0900

    섹션 9 - 값 타입 - 임베디드 타입

--- src/main/java/hellojpa/JpaMainExample.java
+++ src/main/java/hellojpa/JpaMainExample.java
@@ -18,22 +18,12 @@ public class JpaMainExample {
           tx.begin();
 
           try {
-              Child child1 = new Child();
-              Child child2 = new Child();
+              MemberExample member = new MemberExample();
+              member.setUsername("Hello");
+              member.setHomeAddress(new Address("city", "street", "zipcode"));
+              member.setWorkPeriod(new Period());
 
-              Parent parent = new Parent();
-              parent.addChild(child1);
-              parent.addChild(child2);
-
-              em.persist(parent);
-
-              em.flush();
-              em.clear();
-
-              Parent findParent = em.find(Parent.class, parent.getId());
-//              findParent.getChildList().remove(0);
-              em.remove(findParent);
-
+              em.persist(member);
               tx.commit();
           } catch (Exception e) {
               tx.rollback();
--- src/main/java/hellojpa/MemberExample.java
+++ src/main/java/hellojpa/MemberExample.java
@@ -2,11 +2,12 @@ package hellojpa;
 
 import jakarta.persistence.*;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-public class MemberExample extends BaseEntityExample {
+public class MemberExample {
 
     @Id @GeneratedValue
     @Column(name = "Member_ID")
@@ -15,40 +16,27 @@ public class MemberExample extends BaseEntityExample {
     @Column(name = "USERNAME")
     private String username;
 
-    @ManyToOne (fetch = FetchType.LAZY)
-    @JoinColumn
-    private Team team;
+    // 기간 Period
+    @Embedded
+    private Period workPeriod;
 
-//    @OneToOne
-//    @JoinColumn(name = "LOCKER_ID")
-//    private Locker locker;
-//
-//    @OneToMany(mappedBy = "member")
-//    private List<MemberProduct> memberProducts = new ArrayList<>();
+    // 주소
+    @Embedded
+    private Address homeAddress;
 
-    public Team getTeam() {
-        return team;
-    }
-
-    public void setTeam(Team team) {
-        this.team = team;
-    }
-
-//    public Locker getLocker() {
-//        return locker;
-//    }
-//
-//    public void setLocker(Locker locker) {
-//        this.locker = locker;
-//    }
-//
-//    public List<MemberProduct> getMemberProducts() {
-//        return memberProducts;
-//    }
-//
-//    public void setMemberProducts(List<MemberProduct> memberProducts) {
-//        this.memberProducts = memberProducts;
-//    }
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name="city",
+            column = @Column(name = "WORK_CITY")),
+            @AttributeOverride(name = "street",
+            column = @Column(name = "WORK_STREET")),
+            @AttributeOverride(name = "zipcode",
+                    column = @Column(name = "WORK_ZIPCODE"))
+    })
+    private Address workAddress;
+    // 한 엔티티에서 같은 값 타입을 사용하면?
+    // 컬럼 명이 중복됨
+//    @AttributeOverrides, @AttributeOverride를 사용해서 컬럼 명 속성을 재정의
 
     public Long getId() {
         return id;
@@ -66,4 +54,19 @@ public class MemberExample extends BaseEntityExample {
         this.username = username;
     }
 
+    public Period getWorkPeriod() {
+        return workPeriod;
+    }
+
+    public void setWorkPeriod(Period workPeriod) {
+        this.workPeriod = workPeriod;
+    }
+
+    public Address getHomeAddress() {
+        return homeAddress;
+    }
+
+    public void setHomeAddress(Address homeAddress) {
+        this.homeAddress = homeAddress;
+    }
 }
