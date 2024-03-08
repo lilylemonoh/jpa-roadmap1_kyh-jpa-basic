b29c925532eb3d84dd98b2b25cd2c309f7711dbd
lilylemonoh <lilylemonoh@naver.com>
2024-03-08 20:57:07 +0900

    섹션 9 - 실전 예제 6 - 값 타입 매핑

---
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -0,0 +1,63 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+
+@Embeddable
+public class Address {
+
+    @Column(length = 10)
+    private String city;
+    @Column(length = 20)
+    private String street;
+    @Column(length = 5)
+    private String zipcode;
+
+    private String fullAddress() {
+        return getCity() + " " + getStreet()+ " " + getZipcode();
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    private void setCity(String city) {
+        this.city = city;
+    }
+
+    private void setStreet(String street) {
+        this.street = street;
+    }
+
+    private void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Address address = (Address) o;
+
+        if (getCity() != null ? !getCity().equals(address.getCity()) : address.getCity() != null) return false;
+        if (getStreet() != null ? !getStreet().equals(address.getStreet()) : address.getStreet() != null) return false;
+        return getZipcode() != null ? getZipcode().equals(address.getZipcode()) : address.getZipcode() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getCity() != null ? getCity().hashCode() : 0;
+        result = 31 * result + (getStreet() != null ? getStreet().hashCode() : 0);
+        result = 31 * result + (getZipcode() != null ? getZipcode().hashCode() : 0);
+        return result;
+    }
+}
