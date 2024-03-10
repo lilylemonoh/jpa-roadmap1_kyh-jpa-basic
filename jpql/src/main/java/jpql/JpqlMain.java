bf4b1de62058ce6a104c43a1cc8547db2cedf59f
lilylemonoh <lilylemonoh@naver.com>
2024-03-10 21:46:24 +0900

    섹션 10 - 객체지향 쿼리 언어2 - 중급 문법 - 페치 조인 1 - 기본

--- jpql/src/main/java/jpql/JpqlMain.java
+++ jpql/src/main/java/jpql/JpqlMain.java
@@ -15,29 +15,49 @@ public class JpqlMain {
         tx.begin();
 
         try {
-            Team team = new Team();
-            team.setName("teamA");
-            em.persist(team);
+            Team teamA = new Team();
+            teamA.setName("팀A");
+            em.persist(teamA);
 
-            Member member = new Member();
-            member.setUsername("관리자");
-            member.setAge(10);
-            member.setType(MemberType.ADMIN);
+            Team teamB = new Team();
+            teamB.setName("팀B");
+            em.persist(teamB);
 
-            member.setTeam(team);
+            Member member1 = new Member();
+            member1.setUsername("회원1");
+            member1.setTeam(teamA);
+            em.persist(member1);
 
-            em.persist(member);
+            Member member2 = new Member();
+            member2.setUsername("회원2");
+            member2.setTeam(teamA);
+            em.persist(member2);
+
+            Member member3 = new Member();
+            member3.setUsername("회원3");
+            member3.setTeam(teamB);
+            em.persist(member3);
 
             em.flush();
             em.clear();
 
-            String query = "select m.username from Team t join  t.members m";
-            Collection result = em.createQuery(query, Collection.class)
+            String query = "select distinct t from Team t join fetch t.members";
+            List<Team> result = em.createQuery(query, Team.class)
                     .getResultList();
+            System.out.println("result = " + result.size());
 
-            for (Object o : result) {
-                System.out.println("o = " + o);
+            for (Team team : result) {
+                System.out.println("team = " + team.getName() + "|members =" + team.getMembers().size());
+                for (Member member: team.getMembers()) {
+                    System.out.println("member = " + member);
+                }
+                // 회원1, 팀A(SQL)
+                // 회원2, 팀A(1차캐시)
+                // 회원3, 팀B(SQL)
+
+                // 회원 100명 -> N + 1
             }
+
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
